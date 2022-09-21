426f6b77a531843b8f9dce1019f80eabbc269696
Игорь Романенко <113525805+r-igor-s@users.noreply.github.com>
2022-09-21 13:32:42 +0300

    Delete lab4_2.py

--- lab4_2.py
+++
@@ -1,7 +0,0 @@
-import math
-x = float(input("x = "))
-a = float(input("a = "))
-b = float(input("b = "))
-f = (math.pow (x,2) + 0.7)
-y = x * math.tan (x) * math.pow (math.e, a-b) + math.log (math.fabs (f) , 4 )
-print((y))
